a7717b47c541f1e7cd7a2e0296ebd4157fed14fb
androidparanoidal <veiur@yandex.com>
2023-04-03 22:40:51 +0300

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello, Moto!'),
+      ),
+    ),
+  );
+}
