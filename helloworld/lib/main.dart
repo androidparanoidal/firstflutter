85f651ab7efc48701f1567ee8aa2fbf82cd3862e
androidparanoidal <veiur@yandex.com>
2023-04-06 02:12:07 +0300

    Working with Scaffold

--- helloworld/lib/main.dart
+++ helloworld/lib/main.dart
@@ -4,8 +4,19 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello, Moto!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Name'),
+          backgroundColor: Colors.deepPurple[300],
+        ),
+        //body: Center(),
+        backgroundColor: Colors.grey[400],
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'http://3.bp.blogspot.com/-qOJUxvEhU6U/UR9uIryVwkI/AAAAAAAAAHM/zbpMwyNnLOU/s1600/Immagine+010.jpg'),
+          ),
+        ),
       ),
     ),
   );
