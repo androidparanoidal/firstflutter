f944c330950e7d50c8bdc0d4b3c559c9a5686b44
androidparanoidal <veiur@yandex.com>
2023-04-19 15:12:44 +0300

    First commit

---
+++ magicball/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: const Text('Ask Me Anything'),
+          backgroundColor: Colors.blue,
+        ),
+        //body: DicePage(),
+      ),
+    ),
+  );
+}
