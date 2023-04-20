c30fc2109723cf460a630da0e8ceb8a0d803a592
androidparanoidal <veiur@yandex.com>
2023-04-20 16:56:54 +0300

    Step 2

--- magicball/lib/main.dart
+++ magicball/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -28,15 +29,19 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+  int ballNumber = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
       child: TextButton(
         onPressed: () {
-          print("Clicked!");
-          //setState(() {            });
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1; // from 1 to 5
+            print("Clicked!");
+            print("Number: $ballNumber");
+          });
         },
-        child: Image.asset('images/ball1.png'),
+        child: Image.asset('images/ball$ballNumber.png'),
       ),
     );
   }
