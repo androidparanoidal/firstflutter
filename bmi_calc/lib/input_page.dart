514d575a8378ed955420c61f614adc6edceaf2ee
androidparanoidal <veiur@yandex.com>
2023-05-15 23:15:42 +0300

    Add new classes

--- bmi_calc/lib/input_page.dart
+++ bmi_calc/lib/input_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'reusable_card.dart';
+import 'icon_content.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColour = Color(0xFF1D1E33);
@@ -22,9 +25,19 @@ class _InputPageState extends State<InputPage> {
               child: Row(
             children: [
               Expanded(
-                child: ReusableCard(colour: activeCardColour),
+                child: ReusableCard(
+                  colour: activeCardColour,
+                  cardChild:
+                      IconContent(icon: FontAwesomeIcons.mars, label: 'MALE'),
+                ),
               ),
-              Expanded(child: ReusableCard(colour: activeCardColour)),
+              Expanded(
+                child: ReusableCard(
+                  colour: activeCardColour,
+                  cardChild: IconContent(
+                      icon: FontAwesomeIcons.venus, label: 'FEMALE'),
+                ),
+              ),
             ],
           )),
           Expanded(child: ReusableCard(colour: activeCardColour)),
@@ -42,22 +55,6 @@ class _InputPageState extends State<InputPage> {
             height: bottomContainerHeight,
           ),
         ],
-      ),
-    );
-  }
-}
-
-class ReusableCard extends StatelessWidget {
-  //const ReusableCard({super.key});
-  ReusableCard({@required this.colour});
-  final Color? colour;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
