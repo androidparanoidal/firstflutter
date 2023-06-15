e09e7018a6d863b63e0595666ac59c5dbe4b0a88
androidparanoidal <veiur@yandex.com>
2023-06-16 00:07:26 +0300

    Example with ColorTween

--- flashchat/lib/screens/welcome_screen.dart
+++ flashchat/lib/screens/welcome_screen.dart
@@ -21,17 +21,9 @@ class _WelcomeScreenState extends State<WelcomeScreen>
       vsync: this,
     );
 
-    animation = CurvedAnimation(parent: controller, curve: Curves.decelerate);
-
+    animation =
+        ColorTween(begin: Colors.red, end: Colors.blue).animate(controller);
     controller.forward();
-
-    animation.addStatusListener((status) {
-      if (status == AnimationStatus.dismissed) {
-        controller.forward();
-      } else if (status == AnimationStatus.completed) {
-        controller.reverse(from: 1.0);
-      }
-    });
 
     controller.addListener(() {
       setState(() {});
@@ -47,7 +39,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -60,7 +52,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                   tag: 'logo',
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: animation.value * 70,
+                    height: 60.0,
                   ),
                 ),
                 Text(
