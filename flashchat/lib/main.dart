6ee8b8ebf088fe71a8f9b0adc29d609b1cfbb068
androidparanoidal <veiur@yandex.com>
2023-06-30 00:26:39 +0300

    Add Firebase

--- flashchat/lib/main.dart
+++ flashchat/lib/main.dart
@@ -4,7 +4,16 @@ import 'package:flashchat/screens/login_screen.dart';
 import 'package:flashchat/screens/registration_screen.dart';
 import 'package:flashchat/screens/chat_screen.dart';
 
-void main() => runApp(FlashChat());
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
   @override
